b1ff679658b70704ff52f9cd55eb0ff8a61f41cc
prakashtaz0091 <code.ing2468@gmail.com>
2024-04-27 15:27:19 +0545

    production settings

--- project/settings.py
+++ project/settings.py
@@ -144,7 +144,8 @@ if not DEBUG:
     # Enable the WhiteNoise storage backend, which compresses static files to reduce disk use
     # and renames the files with unique names for each version to support long-term caching
     STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
-STATIC_ROOT = '/static/'
+
+STATIC_ROOT = 'static/'
 
 
 MEDIA_URL = 'media/'
